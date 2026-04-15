25fd9e19687b591db8884d6ef515c40a10bb190f
Shanbel Dires <shanbeldires7@gmail.com>
2026-04-15 07:49:28 +0300

    Initialize Express app with middleware and routes

---
+++ Backend/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import errorHandler from "./errorHandler/errorHandler.js";
+import dotenv from "dotenv";
+dotenv.config();
+import { CLIENT_URL } from "./config/env.js";
+import authRoutes from "./routes/authRoute.js";
+const app = express();
+
+// Middlewares
+app.use(cors({ origin:CLIENT_URL, credentials: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(helmet())
+
+// Routes
+app.use("/api/v1/auth", authRoutes);
+
+// Error handler
+app.use(errorHandler);
+
+export default app;
